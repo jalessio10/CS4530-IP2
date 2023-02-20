2c690b35c20e02b0a793a47869ac18e8790cb7ad
Jaiden Alessio <alessiojohn10@gmail.com>
2023-02-20 16:40:50 -0500

    added tests for task 1

--- frontend/src/classes/PosterSessionAreaController.test.ts
+++ frontend/src/classes/PosterSessionAreaController.test.ts
@@ -28,6 +28,49 @@ describe('PosterSessionAreaController', () => {
     testArea.addListener('posterImageContentsChange', mockListeners.posterImageContentsChange);
     testArea.addListener('posterStarChange', mockListeners.posterStarChange);
   });
+  describe('Setting title property', () => {
+    it('updates the property and emits a posterTitleChange event if the property changes', () => {
+      const newTitle = nanoid();
+      testArea.title = newTitle;
+      expect(mockListeners.posterTitleChange).toBeCalledWith(newTitle);
+      expect(testArea.title).toEqual(newTitle);
+    });
+    it('does not emit a posterTitleChange event if the title property does not change', () => {
+      testArea.title = `${testAreaModel.title}`;
+      expect(mockListeners.posterTitleChange).not.toBeCalled();
+    });
+  });
+  describe('Setting posterImageContents property', () => {
+    it('updates the model and emits a posterImageContentsChange event if the property changes', () => {
+      const newImage = nanoid();
+      testArea.imageContents = newImage;
+      expect(mockListeners.posterImageContentsChange).toBeCalledWith(newImage);
+      expect(testArea.imageContents).toEqual(newImage);
+    });
+    it('does not emit a posterImageContentsChange event if the imageContents property does not change', () => {
+      testArea.imageContents = testAreaModel.imageContents;
+      expect(mockListeners.posterImageContentsChange).not.toBeCalled();
+    });
+  });
+  describe('Setting stars property', () => {
+    it('updates the model and emits a posterStarChange event if the property changes', () => {
+      const newStars = testAreaModel.stars + 1;
+      testArea.stars = newStars;
+      expect(mockListeners.posterStarChange).toBeCalledWith(newStars);
+      expect(testArea.stars).toEqual(newStars);
+    });
+    it('does not emit a posterStarChange event if the stars property does not change', () => {
+      const existingValue = testAreaModel.stars;
+      testArea.stars = existingValue;
+      expect(mockListeners.posterStarChange).not.toBeCalled();
+    });
+  });
+  describe('posterSessionAreaModel', () => {
+    it('Carries through all of the properties', () => {
+      const model = testArea.posterSessionAreaModel();
+      expect(model).toEqual(testAreaModel);
+    });
+  });
 
   describe('updateFrom', () => {
     it('Does not update the id property', () => {
